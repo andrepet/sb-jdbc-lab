d9d83a2bafb3e3d6162bca65b4cd863b4983ddac
andrepet <pettersson.andreas.lennart@gmail.com>
2017-09-05 18:39:41 +0200

    Removed unnecessary String arguments from main

--- src/main/java/com/example/JdbcLabApplication.java
+++ src/main/java/com/example/JdbcLabApplication.java
@@ -6,17 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdbcLabApplication{
-
 	public static void main(String[] args) {
-
-		if (args.length >= 4) {
-			String sqlserverIP = args[0];
-			String databasename = args[1];
-			String user = args[2];
-			String password = args[3];
-
-			System.out.printf("%s, %s, %s, %s", sqlserverIP, databasename, user, password);
-		}
 		SpringApplication.run(JdbcLabApplication.class, args);
 	}
 
